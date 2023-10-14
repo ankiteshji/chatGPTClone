164e75b1f58be479827c529d0936259d8a96f528
ankiteshji <ankiteshsingh2@gmail.com>
2023-10-15 00:48:51 +0530

    server updated

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit =async (e)=>{
   loader(messageDiv);
   
   // Fetching the data from server--> Bot's response
-  const response= await fetch('http://localhost:5000',{
+  const response= await fetch('https://chatgptclone-ekgd.onrender.com',{
     method: "POST",
     headers: {
       'Content-Type' :'application/json'
